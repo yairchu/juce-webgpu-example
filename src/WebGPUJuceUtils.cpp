1b9a89259f421690d32759b0762a855e1379d465
Yair Chuchem <yairchu@gmail.com>
2025-09-13 01:56:07 +0300

    Actually copilot was right on JUCE format, support both webgpu pixel formats (by Yair)

--- src/WebGPUJuceUtils.cpp
+++ src/WebGPUJuceUtils.cpp
@@ -15,8 +15,26 @@ void WebGPUJuceUtils::readTextureToImage (WebGPUContext& context, WebGPUTexture&
     const auto src = (uint8_t*) readbackBuffer->getConstMappedRange (0, bytesPerRow * texture.descriptor.size.height);
     juce::Image::BitmapData bitmap (image, juce::Image::BitmapData::writeOnly);
 
-    for (int y = 0; y < (int) texture.descriptor.size.height; ++y)
-        std::memcpy (bitmap.getLinePointer (y), src + y * bytesPerRow, (size_t) texture.descriptor.size.width * 4);
+    switch (texture.descriptor.format)
+    {
+        case WGPUTextureFormat_BGRA8Unorm:
+        case WGPUTextureFormat_BGRA8UnormSrgb:
+            for (int y = 0; y < (int) texture.descriptor.size.height; ++y)
+                std::memcpy (bitmap.getLinePointer (y), src + y * bytesPerRow, (size_t) texture.descriptor.size.width * 4);
+            break;
+        case WGPUTextureFormat_RGBA8Unorm:
+        case WGPUTextureFormat_RGBA8UnormSrgb:
+            for (int y = 0; y < (int) texture.descriptor.size.height; ++y)
+                for (int x = 0; x < (int) texture.descriptor.size.width; ++x)
+                {
+                    const int srcIndex = y * bytesPerRow + x * 4;
+                    bitmap.setPixelColour (x, y, juce::Colour::fromRGBA (src[srcIndex + 0], src[srcIndex + 1], src[srcIndex + 2], src[srcIndex + 3]));
+                }
+            break;
+        default:
+            jassertfalse; // Unsupported format
+            break;
+    }
 
     readbackBuffer->unmap();
 }
