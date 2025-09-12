8d5a1693d8bfd182a1dc91c684072ab1e1d6fdc0
Yair Chuchem <yairchu@gmail.com>
2025-09-13 01:50:36 +0300

    JUCE images are actually RGBA by default contrary to copilot's guess (fix by Yair)
    This should be a faster copy

--- src/WebGPUJuceUtils.cpp
+++ src/WebGPUJuceUtils.cpp
@@ -5,18 +5,18 @@
 
 void WebGPUJuceUtils::readTextureToImage (WebGPUContext& context, WebGPUTexture& texture, juce::Image& image)
 {
+    jassert (texture.descriptor.size.width == (uint32_t) image.getWidth());
+    jassert (texture.descriptor.size.height == (uint32_t) image.getHeight());
+
     wgpu::raii::Buffer readbackBuffer = texture.read (context);
 
     // Copy pixel data (WebGPU uses RGBA, JUCE uses ARGB)
     const int bytesPerRow = texture.bytesPerRow();
     const auto src = (uint8_t*) readbackBuffer->getConstMappedRange (0, bytesPerRow * texture.descriptor.size.height);
     juce::Image::BitmapData bitmap (image, juce::Image::BitmapData::writeOnly);
+
     for (int y = 0; y < (int) texture.descriptor.size.height; ++y)
-        for (int x = 0; x < (int) texture.descriptor.size.width; ++x)
-        {
-            const int srcIndex = y * bytesPerRow + x * 4;
-            bitmap.setPixelColour (x, y, juce::Colour::fromRGBA (src[srcIndex + 0], src[srcIndex + 1], src[srcIndex + 2], src[srcIndex + 3]));
-        }
+        std::memcpy (bitmap.getLinePointer (y), src + y * bytesPerRow, (size_t) texture.descriptor.size.width * 4);
 
     readbackBuffer->unmap();
 }
